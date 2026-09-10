2948f988a1d16ae8598da665b422c8f54526fad0
jose <boxingamateur14@gmail.com>
2026-09-10 09:39:15 -0300

    feat:agregando consultas de funciones de ventana

--- database/02.schema.sql
+++ database/02.schema.sql
@@ -78,3 +78,76 @@ SELECT c.nombre,pf.monto,pf.dias,pf.tna FROM Clientes AS c LEFT JOIN PlazosFijos
 
 
 --FUNCIONES DE AGRUPACION
+--1)OBTENÉ EL TOTAL DEPOSITADO POR TIPO DE TRANSACCIÓN Y CUÁNTAS OPERACIONES SE HICIERON DE CADA UNA.
+SELECT tipo,
+       COUNT(*) AS cantidad_operaciones,
+       SUM(monto) AS total_monto,
+       AVG(monto) AS promedio_monto
+       FROM Transacciones GROUP BY tipo;
+
+--2)Mostra los clientes que tengan más de 1 plazo fijo armado
+SELECT id_cliente, COUNT(*) AS total_plazos_fijos
+    FROM PlazosFijos GROUP BY id_cliente HAVING COUNT(*) > 1;
+
+
+--CASE
+--1)CLASIFICÁ A LOS CLIENTES SEGÚN SU SALDO DISPONIBLE EN CATEGORIAS DE PERFIL DE INVERSOR
+SELECT nombre,saldo,
+    CASE
+        WHEN saldo >= 500000 THEN 'Cliente VIP / Inversor Mayorista'
+        WHEN saldo >= 100000 THEN 'Inversor Moderado'
+        ELSE 'Inversor inicial'
+    END AS perfil_cliente
+    FROM Clientes;
+
+
+--SUBCONSULTAS
+--1)ENCONTRA LOS CLIENTES CUYO SALDO SEA MAYOR AL PROMEDIO GENERAL DE SALDOS DE TODA LA BASE
+SELECT nombre, saldo 
+FROM Clientes 
+WHERE saldo > (SELECT AVG(saldo) FROM Clientes);
+
+
+--FUNCIONES DE Ventana
+--1)rankear los plazos fijos de cada cliente de mayor a menor monto
+SELECT 
+    c.nombre,
+    pf.monto,
+    ROW_NUMBER() OVER (PARTITION BY pf.id_cliente ORDER BY pf.monto DESC) AS pos_consecutiva,
+    RANK()       OVER (PARTITION BY pf.id_cliente ORDER BY pf.monto DESC) AS ranking_con_saltos,
+    DENSE_RANK() OVER (PARTITION BY pf.id_cliente ORDER BY pf.monto DESC) AS ranking_sin_saltos
+FROM PlazosFijos pf
+INNER JOIN Clientes c ON c.id_cliente = pf.id_cliente;
+
+
+--2)calcular el saldo acumulado historial transacción por transacción para un cliente
+SELECT 
+    id_transaccion,
+    id_cliente,
+    fecha,
+    tipo,
+    monto,
+    -- Suma el monto de la transacción actual con todas las anteriores
+    SUM(CASE WHEN tipo = 'DEPOSITO' THEN monto ELSE -monto END) 
+        OVER (PARTITION BY id_cliente ORDER BY fecha, id_transaccion) AS saldo_acumulado
+FROM Transacciones
+WHERE id_cliente = 1;
+
+
+--3)Podés mostrar el monto de un plazo fijo y, en la misma fila, compararlo contra el promedio de plazos fijos de todos los clientes, sin necesidad de hacer un JOIN ni subconsulta
+SELECT 
+    id_plazo_fijo,
+    id_cliente,
+    monto,
+    AVG(monto) OVER () AS promedio_general_sistema,
+    monto - AVG(monto) OVER () AS diferencia_con_promedio
+FROM PlazosFijos;
+
+
+--4)saber cuánto varió el monto entre un plazo fijo y el anterior del mismo cliente
+SELECT 
+    id_cliente,
+    monto AS monto_actual,
+    LAG(monto, 1, 0) OVER (PARTITION BY id_cliente ORDER BY id_plazo_fijo) AS monto_plazo_fijo_anterior,
+    monto - LAG(monto, 1, 0) OVER (PARTITION BY id_cliente ORDER BY id_plazo_fijo) AS diferencia_con_anterior
+FROM PlazosFijos;
